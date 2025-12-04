f2baac5ad1c7ced4d2bf96d17aabe074b3031a32
arshit jain <arshitjain7@gmail.com>
2025-12-04 21:49:38 +0530

    init app or footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,7 +1,47 @@
 import "./footer.css";
+import { assets } from "../../assets/frontend_assets/assets";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer" id="footer ">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img className="footer-img" src={assets.titlelogo} alt="" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+            ipsa obcaecati vel quia commodi quaerat corporis illo, facere
+            architecto quidem explicabo tenetur temporibus eum repellendus!
+            Illum quisquam repellendus accusantium necessitatibus!
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>Company</h2>
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>Get In Touch</h2>
+          <ul>
+            <li>+91-8871807465</li>
+            <li>contact :arshitjain7@gmail.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">
+        CopyRight 2025 FOODZILA.COM -All Right Reserved
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
 import Header from "../../components/Header/Header";
 import "./Home.css";
 import { useState } from "react";
+import AppDownload from "../../components/AppDownload/AppDownload";
 
 const Home = () => {
   const [category, setCategory] = useState("All");
@@ -11,6 +12,7 @@ const Home = () => {
       <Header />
       <ExploreMenu category={category} setCategory={setCategory} />
       <FoodDisplay category={category} />
+      <AppDownload />
     </div>
   );
 };
