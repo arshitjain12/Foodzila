bca1d6a2aab99642ab0de056ab83bda09e5d9daa
arshit jain <arshitjain7@gmail.com>
2025-12-05 14:03:54 +0530

    login-state

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
 import "./LoginPopup.css";
+import { assets } from "../../assets/frontend_assets/assets";
 
-const LoginPopup = () => {
-  return <div>LoginPopup</div>;
+const LoginPopup = ({ setShowLogin }) => {
+  const [currState, setCurrState] = useState("login");
+  return (
+    <div className="login-popup">
+      <form className="login-popup-container">
+        <div className="login-popup-title">
+          <h2>{currState}</h2>
+          <img
+            onClick={() => setShowLogin(false)}
+            src={assets.cross_icon}
+            alt=""
+          />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPopup;
