db482927ddaed963f0d875b245bdb11ddb02061b
arshit jain <arshitjain7@gmail.com>
2025-12-05 18:43:16 +0530

    cart added

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -6,7 +6,10 @@ const Navbar = ({ setShowLogin }) => {
   const [menu, setMenu] = useState("menu");
   return (
     <div className="navbar">
-      <img src={assets.titlelogo} alt="" className="logo" />
+      <Link to="/">
+        <img src={assets.titlelogo} alt="" className="logo" />
+      </Link>
+
       <ul className="navbar-menu">
         <Link
           to="/"
@@ -40,7 +43,11 @@ const Navbar = ({ setShowLogin }) => {
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
-          <img src={assets.basket_icon} alt="" />
+          <Link to="/cart">
+            {" "}
+            <img src={assets.basket_icon} alt="" />
+          </Link>
+
           <div className="dot"> </div>
         </div>
         <button onClick={() => setShowLogin(true)}>sign in</button>
